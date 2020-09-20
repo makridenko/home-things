b77ef8e6af6a343d68516728c908f4b7da718fe9
makridenko <makridenko.a@yandex.ru>
2020-09-21 01:04:25 +0300

    add form to each model;

--- core/house/models/place.py
+++ core/house/models/place.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django import forms
 
 
 class Place(models.Model):
@@ -18,3 +19,9 @@ class Place(models.Model):
 
     def __str__(self):
         return f'{self.room}/{self.title}'
+
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = '__all__'
